a7b203cf756219d4f3eb4bedfa06155e3419e072
Your Name <you@example.com>
2022-02-27 16:43:06 +0600

    list type

---
+++ Py_list.py
@@ -0,0 +1,14 @@
+a = ["name", "age", "roll"]
+print(a)
+print(type(a))
+w = ["name", 1,3,4.5]
+print(w)
+
+b = list(('kamal', 'jamal','manik'))
+print(b)
+w = ["name",'jamal', "hanif",["name",'age','id'], "salam"]
+print(w)
+print(type(w[2:5]))
+print(w[3][2][0])
+print(w[3][-2])
+print(b[-3][-1])
