202e803707c7f4033f51d1436a3a54df04640862
Your Name <you@example.com>
2022-06-30 23:44:52 +0600

    add new file

---
+++ datatype.py
@@ -0,0 +1,12 @@
+a = 10
+print(a)
+
+# a =None
+a =20
+print(a)
+print(type(a))
+if a ==None:
+    print("yes none")
+else:
+    print("Not none")
+
--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
 print("hello world")
+for i in range(1,6):
+    if i ==3:
+        continue
+    print(i)
+
+i = 1
+while i<=5:
+    if i ==4:
+        continue
+    print(i)
+    i += 1
+
